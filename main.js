5576d07e95668b51b54acf3b2a3272667406763a
Revvvolution <huffman40@mctc.edu>
2024-02-04 20:06:03 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hello, world!")
+
+let myName = "Dwayne The Rock Johnson"; // variable assignment
+
+let myName; // variable declaration
+myName = "Dwayne The Rock Johnson"; // variable assignment
+
+// We declare the variable just like we did before
+let myName = "Dwayne The Rock Johnson";
+
+// Except this time we reassign it to hold a different value. Notice we don't have to use the let keyword for reassignments.
+myName = "The Rock";
+
+// This should log "The Rock" to the console
+console.log(myName)
